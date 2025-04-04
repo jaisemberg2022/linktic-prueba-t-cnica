dc5273d81335dc99b413e1a4ad5dccb1c067042b
jaisemberg2022 <jaisembergll@gmail.com>
2025-04-04 18:05:02 -0500

    intalacion y configuracion global de jest y testing react ademas de prubas unitarias realizadas a pagina index y componente asociado

--- src/pages/albums/services/AlbumsService.ts
+++ src/pages/albums/services/AlbumsService.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { AlbumInfo } from "../interface/AlbumsIntrface";
 
 export const getAlbums = async (filter: AlbumInfo) => {
-    console.log({serv: filter})
     const response = await axios.get("https://jsonplaceholder.typicode.com/albums", {
         params: {
             userId: filter.userId,
